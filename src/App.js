3a887c3943ddd3583c1da4e376393c51e8273ab0
Wan Nabil <wannabil.xyz@gmail.com>
2023-05-05 13:45:56 +0800

    final

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const UserList = [
+  {
+    id: 1,
+    name: 'John Doe',
+    email: 'johndoe@example.com',
+  },
+  {
+    id: 2,
+    name: 'Jane Smith',
+    email: 'janesmith@example.com',
+  }
+];
+
 function App() {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [filteredUsers, setFilteredUsers] = useState(UserList);
+
+  const handleChange = (e) => {
+    setSearchTerm(e.target.value);
+    filterUsers(e.target.value);
+  };
+
+  const filterUsers = (searchValue) => {
+    const filteredList = UserList.filter((user) =>
+      user.name.toLowerCase().includes(searchValue.toLowerCase())
+    );
+    setFilteredUsers(filteredList);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form>
+          <input className="form-control form-control-lg" type="text" placeholder="Search Here" value={searchTerm} onChange={handleChange} />
+        </form>
+        <ul className="UserList">
+          {filteredUsers.map((user) => (
+            <li key={user.id}>
+              <p className="UserName">{user.name}</p>
+              <p className="UserEmail">{user.email}</p>
+            </li>
+          ))}
+        </ul>
       </header>
     </div>
   );
 }
 
 export default App;
+
+
+
+
+
